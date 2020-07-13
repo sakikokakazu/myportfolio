0fb1405c71a8407b2954e308c890f820659bf914
sakikokakazu <44572335+sakikokakazu@users.noreply.github.com>
2020-07-13 21:17:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
     });
 
     var typed = new Typed(".typed", {
-        strings: ["Software Enginner.", "Web Developer.", "Web Designer."],
+        strings: ["Software Enginner.", "Web Developer.", "Student."],
         typeSpeed: 70,
         loop: true,
         startDelay: 1000,
